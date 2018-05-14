573661cd8cffc45ada92e2b91638610f347a4f93
Unknown <diengadama1@gmail.com>
2018-05-14 22:00:46 +0200

    test

---
+++ helloWorld/src/App.java
@@ -0,0 +1,19 @@
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+    
+    public static boolean coverageMethod(boolean val) {
+    	if(val){
+    		return true;
+    	} else {
+    		return false;
+    	}
+    }
+}
